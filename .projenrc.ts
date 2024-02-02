bdec2fdecf23f62d5e42e1bb8115f6a54d292b48
Gary Sassano <10464497+garysassano@users.noreply.github.com>
2024-02-02 01:44:36 +0100

    update

--- .projenrc.ts
+++ .projenrc.ts
@@ -1,19 +1,19 @@
 import { javascript } from "projen";
 import { CdktfTypeScriptApp } from "projen-cdktf-app-ts";
 const project = new CdktfTypeScriptApp({
-  cdktfVersion: "0.20.1",
+  cdktfVersion: "0.20.3",
   defaultReleaseBranch: "main",
   depsUpgradeOptions: { workflow: false },
   devDeps: ["projen-cdktf-app-ts"],
   eslint: true,
   name: "cdktf-project",
   packageManager: javascript.NodePackageManager.PNPM,
-  pnpmVersion: "8.14.2",
+  pnpmVersion: "8.15.1",
   prettier: true,
   projenrcTs: true,
 
   terraformProviders: [
-    "hashicorp/aws@~> 5.33.0",
+    "hashicorp/aws@~> 5.34.0",
     "kreuzwerker/docker@~> 3.0.2",
   ],
 });
